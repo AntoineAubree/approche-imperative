c021b9eccd09f339e7c7110b9e34c4bd011867e6
DESKTOP-J4CVPG5\Antoine <antoine.aubree@protonmail.com>
2021-01-25 15:05:45 +0100

    add InteractifTableMulti

---
+++ src/fr/algorithmie/InteractifTableMult.java
@@ -0,0 +1,14 @@
+package fr.algorithmie;
+
+public class InteractifTableMult {
+
+	public static void main(String[] args) {
+		int userNumber = InteractifTantQue.enterNumber(1, 10);
+
+		// display multiplication table of userNumber
+		for (int i = 1; i < 11; i++) {
+			System.out.println(userNumber + " * " + i + " = " + userNumber * i);
+		}
+	}
+
+}
--- src/fr/algorithmie/InteractifTantQue.java
+++ src/fr/algorithmie/InteractifTantQue.java
@@ -5,14 +5,25 @@ import java.util.Scanner;
 public class InteractifTantQue {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		int userNumber = enterNumber(1, 10);
+		System.out.println("Votre nombre est : " + userNumber);
+	}
+
+	/**
+	 * 
+	 * @param minValue minimum value that the user is allowed to enter
+	 * @param maxValue maximum value that the user is allowed to enter
+	 * @return integer enter by user
+	 */
+	protected static int enterNumber(int minValue, int maxValue) {
 		int userNumber = 0;
-		while (userNumber < 1 || userNumber > 10) {
+		Scanner sc = new Scanner(System.in);
+		while (userNumber < minValue || userNumber > maxValue) {
 			System.out.println("Veuillez saisir un nombre compris entre 1 et 10 :");
 			userNumber = sc.nextInt();
 		}
-		System.out.println("Votre nombre est : " + userNumber);
 		sc.close();
+		return userNumber;
 	}
 
 }
