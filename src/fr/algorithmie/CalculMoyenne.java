f33b19f7484e59e5e8c1e98fd3bc9a287309b70c
DESKTOP-J4CVPG5\Antoine <antoine.aubree@protonmail.com>
2021-01-25 13:05:54 +0100

    add ComparaisonTableau

--- src/fr/algorithmie/CalculMoyenne.java
+++ src/fr/algorithmie/CalculMoyenne.java
@@ -5,7 +5,7 @@ public class CalculMoyenne {
 	public static void main(String[] args) {
 		int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
 		
-		// clacul of the array elements average
+		// calculation of the array elements average
 		int sumArrayElements = 0;
 		float averageArrayElements = 0F;
 		for (int i = 0; i < array.length; i++) {
